48e96bbf3d92c7ee29628acf2e9488dd37e02307
javiblancotorres <javiblancotorres@gmail.com>
2014-10-20 18:51:52 +0200

    Con columnas

---
+++ Flappy Pájaro/Assets/Scripts/movimientoScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movimientoScript : MonoBehaviour {
+
+	public int vhorizontal = 10;
+	public int vvertical = 10;
+	//esto de abajo significa que vas a crear una variable llamada movimiento, de tipo Vector3
+	Vector3 movimiento;
+	Vector3 posicionRaton;
+
+
+	// Update is called once per frame
+	void Update () {
+		 //GetAxis te da valores entre 0 y 1. De manera que hara que la cosa funcione mas o menos como la sensibilidad del joystick de las consolas
+
+		//var v = Input.GetAxis ("Vertical");
+		//var h = Input.GetAxis ("Horizontal"); 
+
+		posicionRaton = Input.mousePosition;
+
+		//La frase de abajo es muy importante, ya que te transforma las coordenadas del raton en la pantalla al raton en la camara.
+		posicionRaton = Camera.main.ScreenToWorldPoint (posicionRaton);
+
+		//movimiento = new Vector3 (vhorizontal * h, vvertical * v, 0);
+	 
+		//transform.Translate (movimiento * Time.deltaTime);
+
+		if (Input.GetMouseButton (0)) {
+						transform.position = Vector2.Lerp (transform.position, posicionRaton, vvertical * 0.1f);
+				}
+
+
+	}
+}
